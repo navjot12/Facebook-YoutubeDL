bdc2a7f7129fff96bbd458e4531eb8d0d3a1aaa7
nsa12 <singh.navjot12@gmail.com>
2016-12-18 20:03:15 +0530

    first blood

---
+++ manager/views.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from django.shortcuts import render
+from django.http import HttpResponse
+
+from django.views import generic
+from django.views.decorators.csrf import csrf_exempt
+from django.utils.decorators import method_decorator
+import json
+import requests
+
+VERIFY_TOKEN = 'youtube-download-karega'
+PAGE_ACCESS_TOKEN = 'EAAO5LXdwYSwBAFNtQwyXBAgswtxV9wVMQMoUO887BT4dE8qFykRoyqEftoe2GHJe35HuLHL8ZAPmWWoW4evqBTO6cYUdFO7CYqKtyBLXvMrIxApNQe5iZBRmC3S6g0HEZBKOwzZAG0OXSrcZBGMcBlEHtKO57ownY3cDvAYMevwZDZD'
+
+def post_facebook_message(fbid, message_text):
+	post_message_url = 'https://graph.facebook.com/v2.6/me/messages?access_token=%s'%PAGE_ACCESS_TOKEN
+	response_msg = json.dumps({"recipient":{"id":fbid}, "message":{"text":message_text}})
+	status = requests.post(post_message_url, headers={"Content-Type": "application/json"},data=response_msg)
+	print status.json()
+
+
+class MyChatBotView(generic.View):
+	def get (self, request, *args, **kwargs):
+		if self.request.GET['hub.verify_token'] == VERIFY_TOKEN:
+			return HttpResponse(self.request.GET['hub.challenge'])
+		else:
+			return HttpResponse('Oops invalid token')
+
+	@method_decorator(csrf_exempt)
+	def dispatch(self, request, *args, **kwargs):
+		return generic.View.dispatch(self, request, *args, **kwargs)
+
+	def post(self, request, *args, **kwargs):
+		incoming_message= json.loads(self.request.body.decode('utf-8'))
+		print incoming_message
+
+		for entry in incoming_message['entry']:
+			for message in entry['messaging']:
+				print message
+				try:
+					sender_id = message['sender']['id']
+					message_text = message['message']['text']
+					post_facebook_message(sender_id,message_text) 
+				except Exception as e:
+					print e
+					pass
+
+		return HttpResponse()
+
+def index(request):
+	return HttpResponse('Building Youtube Downloader Bot!')
