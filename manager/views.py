cb2aefdf4f9792bc55ea907a977e163435bc498b
nsa12 <singh.navjot12@gmail.com>
2016-12-18 23:43:27 +0530

    work man

--- manager/views.py
+++ manager/views.py
@@ -8,11 +8,7 @@ from django.views import generic
 from django.views.decorators.csrf import csrf_exempt
 from django.utils.decorators import method_decorator
 import json
-import sys
-import os
-import time
 import requests
-from bs4 import BeautifulSoup as BS
 
 VERIFY_TOKEN = 'youtube-download-karega'
 PAGE_ACCESS_TOKEN = 'EAAO5LXdwYSwBAFNtQwyXBAgswtxV9wVMQMoUO887BT4dE8qFykRoyqEftoe2GHJe35HuLHL8ZAPmWWoW4evqBTO6cYUdFO7CYqKtyBLXvMrIxApNQe5iZBRmC3S6g0HEZBKOwzZAG0OXSrcZBGMcBlEHtKO57ownY3cDvAYMevwZDZD'
@@ -23,8 +19,6 @@ def post_facebook_message(fbid, message_text):
 	status = requests.post(post_message_url, headers={"Content-Type": "application/json"},data=response_msg)
 	print status.json()
 
-def sendAudio(fbid, file_name):
-	pass
 
 class MyChatBotView(generic.View):
 	def get (self, request, *args, **kwargs):
@@ -47,14 +41,6 @@ class MyChatBotView(generic.View):
 				try:
 					sender_id = message['sender']['id']
 					message_text = message['message']['text']
-					
-					words = message_text.split(' ')
-					flag_URL = 0
-					title = ''
-					for text in words:
-						if text.startswith('https://') or text.startswith('www.') or text.startswith('youtu'):
-							message_text = 'Youtube URL detected!'
-
 					post_facebook_message(sender_id,message_text) 
 				except Exception as e:
 					print e
