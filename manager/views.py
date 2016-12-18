dbd093b34122fd5d9be0ab15e63ef0a65ba1ec24
nsa12 <singh.navjot12@gmail.com>
2016-12-19 02:39:06 +0530

    finale

--- manager/views.py
+++ manager/views.py
@@ -16,11 +16,42 @@ import pafy
 VERIFY_TOKEN = 'youtube-download-karega'
 PAGE_ACCESS_TOKEN = 'EAAO5LXdwYSwBAFNtQwyXBAgswtxV9wVMQMoUO887BT4dE8qFykRoyqEftoe2GHJe35HuLHL8ZAPmWWoW4evqBTO6cYUdFO7CYqKtyBLXvMrIxApNQe5iZBRmC3S6g0HEZBKOwzZAG0OXSrcZBGMcBlEHtKO57ownY3cDvAYMevwZDZD'
 
+def set_greeting_text():
+	post_message_url = "https://graph.facebook.com/v2.6/me/thread_settings?access_token=%s"%PAGE_ACCESS_TOKEN
+	request_msg = {
+		"setting_type":"greeting",
+		"greeting":{
+			"text":"This chatbot allows you to download youtube videos in the best quality!\nJust paste the url of a youtube video to download it's audio. Or enter \"<video url> video\" to download the video."
+		}
+	}
+	status = requests.post(post_message_url, headers={"Content-Type": "application/json"}, data=response_msg_quikreply)
+	print status
+
 def post_facebook_message(fbid, message_text):
 	post_message_url = 'https://graph.facebook.com/v2.6/me/messages?access_token=%s'%PAGE_ACCESS_TOKEN
 	response_msg = json.dumps({"recipient":{"id":fbid}, "message":{"text":message_text}})
 	status = requests.post(post_message_url, headers={"Content-Type": "application/json"},data=response_msg)
 	print status.json()
+
+def post_facebook_audio(fbid, message_text):
+	post_message_url = 'https://graph.facebook.com/v2.6/me/messages?access_token=%s'%PAGE_ACCESS_TOKEN
+
+	response_msg_audio = {
+		"recipient":{
+			"id":fbid
+		},
+		"message":{
+			"attachment":{
+		    	"type":"audio",
+		    	"payload":{
+		        	"url":message_text
+		      	}
+		    }
+		}
+	}
+	response_msg_audio = json.dumps(response_msg_audio)
+	status = requests.post(post_message_url, headers={"Content-Type": "application/json"}, data=response_msg_audio)
+	print status
 
 
 class MyChatBotView(generic.View):
@@ -69,16 +100,17 @@ class MyChatBotView(generic.View):
 							message_text = 'Will download video'
 							r = requests.get('http://tinyurl.com/api-create.php?url=' + best.url)
 							message_text = 'Download Video: ' + str(r.text)
-							post_facebook_message(sender_id,message_text)
+							post_facebook_message(sender_id, message_text)
 						else:
 							message_text = 'Will download audio'
 							bestaudio = video.getbestaudio(preftype="m4a")
 							r = requests.get('http://tinyurl.com/api-create.php?url=' + bestaudio.url)
 							message_text = 'Download Audio: ' + str(r.text)
 							post_facebook_message(sender_id,message_text)
+							post_facebook_audio(sender_id, bestaudio.url)
 							message_text = 'IMPORTANT: After downloading, rename the file to (anyname).m4a.\nNOTE: You could also save in .mp3 extension, but m4a provides better quality!'
 							post_facebook_message(sender_id,message_text)
-							
+
 				except Exception as e:
 					print e
 					pass
@@ -86,4 +118,5 @@ class MyChatBotView(generic.View):
 		return HttpResponse()
 
 def index(request):
+	set_greeting_text()
 	return HttpResponse('Building Youtube Downloader Bot!')
