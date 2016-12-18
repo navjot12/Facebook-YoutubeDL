fe6ed2ff8c38005864d783c8963a99d51bad6ac0
nsa12 <singh.navjot12@gmail.com>
2016-12-19 04:18:32 +0530

    lets see

--- manager/views.py
+++ manager/views.py
@@ -18,14 +18,71 @@ PAGE_ACCESS_TOKEN = 'EAAO5LXdwYSwBAFNtQwyXBAgswtxV9wVMQMoUO887BT4dE8qFykRoyqEfto
 
 def set_greeting_text():
 	post_message_url = "https://graph.facebook.com/v2.6/me/thread_settings?access_token=%s"%PAGE_ACCESS_TOKEN
-	request_msg = {
+	response_msg = {
 		"setting_type":"greeting",
 		"greeting":{
 			"text":"This chatbot allows you to download youtube videos in the best quality!\nJust paste the url of a youtube video to download it's audio. Or enter \"<video url> video\" to download the video."
 		}
 	}
+	response_msg = json.dumps(response_msg)
 	status = requests.post(post_message_url, headers={"Content-Type": "application/json"}, data=response_msg)
 	print status
+
+def post_facebook_quickreply(fbid, message_text):
+	post_message_url = "https://graph.facebook.com/v2.6/me/thread_settings?access_token=%s"%PAGE_ACCESS_TOKEN
+	response_msg_quickreply = {
+		"recipient":{
+		    "id":fbid
+		  },
+		"message":{
+			"text":"What would you like to download?",
+		    "quick_replies":[
+		    {
+		    	"content_type":"text",
+		        "title":'Audio',
+		        "payload":'Audio :' + message_text
+		    },
+		    {
+			    "content_type":"text",
+		    	"title":'Video',
+		    	"payload":'Video :' + message_text
+		    }
+		    ]
+		}
+	}
+	response_msg_quickreply = json.dumps(response_msg_quickreply)
+	status = requests.post(post_message_url, headers={"Content-Type": "application/json"}, data=response_msg_quickreply)
+	print status.json()
+
+def handle_quickreply(fbid, payload):
+	post_message_url = 'https://graph.facebook.com/v2.6/me/messages?access_token=%s'%PAGE_ACCESS_TOKEN
+	
+	url = payload.split(':')[1]
+	video = pafy.new(url)
+	best = video.getbest()
+	message_text = video.title + '\t(' + video.duration + ')'
+	post_facebook_message(sender_id,message_text)
+	
+	if payload.split(':')][0] == 'Video':
+		r = requests.get('http://tinyurl.com/api-create.php?url=' + best.url)
+		message_text = 'Download Video: ' + str(r.text)
+		post_facebook_message(sender_id, message_text)
+		message_text = 'Open the link, right click on the video to save it.'
+		post_facebook_message(sender_id,message_text)
+		#post_facebook_video(sender_id, best.url)
+		post_facebook_file(sender_id, best.url)
+	
+	elif payload.split(':')][0] == 'Audio':
+		bestaudio = video.getbestaudio(preftype="m4a")
+		r = requests.get('http://tinyurl.com/api-create.php?url=' + bestaudio.url)
+		post_facebook_audio(sender_id, bestaudio.url)
+		message_text = 'Download Audio: ' + str(r.text)
+		post_facebook_message(sender_id,message_text)
+		message_text = 'Open the link, right click on the audio and while saving, rename it to (anything).m4a.\nNOTE: You could also save in .mp3 extension, but m4a provides better quality!'
+		post_facebook_message(sender_id,message_text)
+		post_facebook_file(sender_id, bestaudio.url)
+	
+	return
 
 def post_facebook_message(fbid, message_text):
 	post_message_url = 'https://graph.facebook.com/v2.6/me/messages?access_token=%s'%PAGE_ACCESS_TOKEN
@@ -109,47 +166,34 @@ class MyChatBotView(generic.View):
 
 		for entry in incoming_message['entry']:
 			for message in entry['messaging']:
-				print message
+				sender_id = message['sender']['id']
 				try:
-					sender_id = message['sender']['id']
+					if 'quick_reply' in message['message']:
+						handle_quickreply(sender_id, message['message']['quick_reply']['payload'])
+					else:
+						pass
+				except Exception as e:
+					print e
+					pass
+				
+				try:
 					message_text = message['message']['text']
 
 					words = message_text.split(' ')
 					flag_URL = 0
-					flag_VIDEO = 0
 					url = '_'
 
 					for word in words:
 						if word.startswith('https://') or word.startswith('www.') or word.startswith('youtu'):
 							url = word
 							flag_URL = 1
-						elif word.lower().startswith('video'):
-							flag_VIDEO = 1
-					
+
 					if flag_URL == 0:
 						message_text = 'Please enter a valid video link to download.'
 						post_facebook_message(sender_id, message_text)
 
 					else:
-						video = pafy.new(url)
-						best = video.getbest()
-						message_text = video.title + '\t(' + video.duration + ')'
-						post_facebook_message(sender_id,message_text)
-						if flag_VIDEO == 1:
-							r = requests.get('http://tinyurl.com/api-create.php?url=' + best.url)
-							message_text = 'Download Video: ' + str(r.text)
-							post_facebook_message(sender_id, message_text)
-							#post_facebook_video(sender_id, best.url)
-							post_facebook_file(sender_id, best.url)
-						else:
-							bestaudio = video.getbestaudio(preftype="m4a")
-							r = requests.get('http://tinyurl.com/api-create.php?url=' + bestaudio.url)
-							post_facebook_audio(sender_id, bestaudio.url)
-							message_text = 'Download Audio: ' + str(r.text)
-							post_facebook_message(sender_id,message_text)
-							message_text = 'IMPORTANT: After downloading, rename the file to (anyname).m4a.\nNOTE: You could also save in .mp3 extension, but m4a provides better quality!'
-							post_facebook_message(sender_id,message_text)
-							post_facebook_file(sender_id, bestaudio.url)
+						post_facebook_quickreply(sender_id, url)
 
 				except Exception as e:
 					print e
