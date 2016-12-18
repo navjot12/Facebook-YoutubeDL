f01e1b99f6b45eb26d5be18a0406acb3237f16cb
nsa12 <singh.navjot12@gmail.com>
2016-12-18 23:42:00 +0530

    work bro

--- manager/views.py
+++ manager/views.py
@@ -9,10 +9,10 @@ from django.views.decorators.csrf import csrf_exempt
 from django.utils.decorators import method_decorator
 import json
 import sys
+import os
 import time
-import telepot
 import requests
-import pafy
+from bs4 import BeautifulSoup as BS
 
 VERIFY_TOKEN = 'youtube-download-karega'
 PAGE_ACCESS_TOKEN = 'EAAO5LXdwYSwBAFNtQwyXBAgswtxV9wVMQMoUO887BT4dE8qFykRoyqEftoe2GHJe35HuLHL8ZAPmWWoW4evqBTO6cYUdFO7CYqKtyBLXvMrIxApNQe5iZBRmC3S6g0HEZBKOwzZAG0OXSrcZBGMcBlEHtKO57ownY3cDvAYMevwZDZD'
@@ -22,6 +22,9 @@ def post_facebook_message(fbid, message_text):
 	response_msg = json.dumps({"recipient":{"id":fbid}, "message":{"text":message_text}})
 	status = requests.post(post_message_url, headers={"Content-Type": "application/json"},data=response_msg)
 	print status.json()
+
+def sendAudio(fbid, file_name):
+	pass
 
 class MyChatBotView(generic.View):
 	def get (self, request, *args, **kwargs):
@@ -47,41 +50,12 @@ class MyChatBotView(generic.View):
 					
 					words = message_text.split(' ')
 					flag_URL = 0
-					flag_VIDEO = 0
-					title = '_'
-					url = '_'
-
+					title = ''
 					for text in words:
 						if text.startswith('https://') or text.startswith('www.') or text.startswith('youtu'):
-							url = text
-							flag_URL = 1
-						elif text.lower().startswith('video'):
-							flag_VIDEO = 1
-					
-					if flag_URL == 0:
-						message_text = 'Please enter a video link to download.'
+							message_text = 'Youtube URL detected!'
 
-					else:
-						video = pafy.new(url)
-						best = video.getbest()
-						message_text = video.title + '\t(' + video.duration + ')'
-						post_facebook_message(sender_id,message_text)
-						
-						if flag_VIDEO == 1:
-						    r = requests.get('http://tinyurl.com/api-create.php?url=' + best.url)
-						    message_text = message_text + '\n\nDownload Video: ' + str(r.text)
-						    #post_facebook_message(sender_id,message_text)
-
-						else :
-							bestaudio = video.getbestaudio(preftype="m4a")
-							r = requests.get('http://tinyurl.com/api-create.php?url=' + bestaudio.url)
-							message_text = message_text + '\n\nDownload Audio: ' + str(r.text)
-							#post_facebook_message(sender_id,message_text)
-							message_text = message_text + '\n\nIMPORTANT: After downloading, rename the file to (anyname).m4a.\nNOTE: You could also save in .mp3 extension, but m4a provides better quality!'
-							#post_facebook_message(sender_id,message_text)
-
-					post_facebook_message(sender_id,message_text)
-						    
+					post_facebook_message(sender_id,message_text) 
 				except Exception as e:
 					print e
 					pass
