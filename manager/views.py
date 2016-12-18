94e8ccadc72ada4556015230f353969f5ef334cd
nsa12 <singh.navjot12@gmail.com>
2016-12-18 23:49:10 +0530

    isko chalao

--- manager/views.py
+++ manager/views.py
@@ -8,7 +8,11 @@ from django.views import generic
 from django.views.decorators.csrf import csrf_exempt
 from django.utils.decorators import method_decorator
 import json
+import sys
+import time
+import telepot
 import requests
+import pafy
 
 VERIFY_TOKEN = 'youtube-download-karega'
 PAGE_ACCESS_TOKEN = 'EAAO5LXdwYSwBAFNtQwyXBAgswtxV9wVMQMoUO887BT4dE8qFykRoyqEftoe2GHJe35HuLHL8ZAPmWWoW4evqBTO6cYUdFO7CYqKtyBLXvMrIxApNQe5iZBRmC3S6g0HEZBKOwzZAG0OXSrcZBGMcBlEHtKO57ownY3cDvAYMevwZDZD'
@@ -41,6 +45,27 @@ class MyChatBotView(generic.View):
 				try:
 					sender_id = message['sender']['id']
 					message_text = message['message']['text']
+					
+					words = message_text.split(' ')
+					flag_URL = 0
+					flag_VIDEO = 0
+					url = '_'
+					for word in words:
+						if text.startswith('https://') or text.startswith('www.') or text.startswith('youtu'):
+							url = word
+							flag_URL = 1
+						elif text.lower().startswith('video'):
+							flag_VIDEO = 1
+
+					if flag_URL == 0:
+						message_text = 'Please enter a valid video link to download.'
+
+					else:
+						if flag_VIDEO == 1:
+							message_text = 'Will download video'
+						else:
+							message_text = 'Will download audio'
+					
 					post_facebook_message(sender_id,message_text) 
 				except Exception as e:
 					print e
