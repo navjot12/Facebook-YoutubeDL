58219024756e09735cccfffbfb71e71721fb1c00
nsa12 <singh.navjot12@gmail.com>
2016-12-23 01:00:39 +0530

    Minor change in message

--- manager/views.py
+++ manager/views.py
@@ -13,8 +13,8 @@ import requests
 import pafy
 from bs4 import BeautifulSoup as BS
 
-VERIFY_TOKEN = 'insert-your-own'
-PAGE_ACCESS_TOKEN = 'insert-your-own'
+VERIFY_TOKEN = 'youtube-download-karega'
+PAGE_ACCESS_TOKEN = 'EAAO5LXdwYSwBADZClxlG8zxcgHdcmzhr87ZC6H3wvWQyypX1666JRcEJwhIk830av89OGoqtkogM0tJS74vQElsMyaKo9i1lG5J0GIAF9nfFQiSeyxjkkWJDRX8ZBdYeFujPujW7DRCjzZA8XGuN7d6o1SbXYLPZBa4kvForUJgZDZD'
 
 def scraper(search):
 	url = "https://www.youtube.com/results?search_query="
