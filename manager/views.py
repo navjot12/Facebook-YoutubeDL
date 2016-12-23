03a054799bfeca7a3575ca46b1e494451efc3fc3
nsa12 <singh.navjot12@gmail.com>
2016-12-24 02:00:11 +0530

    Minor URL adjustment for Mobile YouTube links

--- manager/views.py
+++ manager/views.py
@@ -386,6 +386,7 @@ class MyChatBotView(generic.View):
 							if word.startswith('https://') or word.startswith('http://') or word.startswith('www.') or word.startswith('youtu'):
 								flag_URL = 1
 								url = word
+								url.replace("m.you", "you")
 							if word.lower() in ['audio','video']:
 								flag_AV = 1
 								AV = word.lower()
