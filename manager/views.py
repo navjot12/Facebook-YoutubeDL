c8db485f19df89f8294af78f842f858d482a90d0
nsa12 <singh.navjot12@gmail.com>
2016-12-26 04:23:55 +0530

    Abandoning Files Method

--- manager/views.py
+++ manager/views.py
@@ -46,14 +46,13 @@ def scraper(search):
 		thumbnail = video.find('div', {'class': 'yt-lockup-thumbnail contains-addto'})
 		data = video.find('div', {'class': 'yt-lockup-content'})
 		try:
-			uploader = data.find('div', {'class': 'yt-lockup-byline'}).find('a').text.decode('utf-8', 'strict')
-			heading = data.find_all('h3', {'class': 'yt-lockup-title'})[0].find_all('a')[0].text.decode('utf-8', 'strict')
-			print heading
-			url = 'https://www.youtube.com' + data.find('h3', {'class': 'yt-lockup-title'}).find('a')['href'].decode('utf-8', 'strict')
-			duration = data.find('h3', {'class': 'yt-lockup-title'}).find('span').text.decode('utf-8', 'strict')
-			uploaded_on = data.find('div', {'class': 'yt-lockup-meta'}).find('ul').find_all('li')[0].text.decode('utf-8', 'strict')
-			views = data.find('div', {'class': 'yt-lockup-meta'}).find('ul').find_all('li')[1].text.decode('utf-8', 'strict')
-			image = thumbnail.find('span', {'class': 'yt-thumb-simple'}).find('img')['src'].decode('utf-8', 'strict')
+			uploader = data.find('div', {'class': 'yt-lockup-byline'}).find('a').text.decode('utf-8', 'ignore')
+			heading = data.find_all('h3', {'class': 'yt-lockup-title'})[0].find_all('a')[0].text.decode('utf-8', 'ignore')
+			url = 'https://www.youtube.com' + data.find('h3', {'class': 'yt-lockup-title'}).find('a')['href'].decode('utf-8', 'ignore')
+			duration = data.find('h3', {'class': 'yt-lockup-title'}).find('span').text.decode('utf-8', 'ignore')
+			uploaded_on = data.find('div', {'class': 'yt-lockup-meta'}).find('ul').find_all('li')[0].text.decode('utf-8', 'ignore')
+			views = data.find('div', {'class': 'yt-lockup-meta'}).find('ul').find_all('li')[1].text.decode('utf-8', 'ignore')
+			image = thumbnail.find('span', {'class': 'yt-thumb-simple'}).find('img')['src'].decode('utf-8', 'ignore')
 		except Exception as e:
 			print '\n\nError aagya\n'
 			print e
