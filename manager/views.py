1d6b4fed7df94f06698bd4c774c2351c4ddaac6e
Navjot Singh <singh.navjot12@gmail.com>
2017-02-26 02:53:11 +0530

    Trying to send audio and file too

--- manager/views.py
+++ manager/views.py
@@ -170,8 +170,12 @@ def handle_quickreply(sender_id, payload):
 	elif payload.split('!$#@')[0] == 'audio':
 		url2 = url.split('watch?v=')[1]
 		audiolink = scraper2(url2)
-		#post_facebook_file(sender_id, audiolink)			#Don't know why this isn't working, sending a legit audio file
-		#post_facebook_audio(sender_id, audiolink)			#Don't know why this isn't working, sending a legit audio file
+		
+		print '^'*15 + '\n'
+		post_facebook_file(sender_id, audiolink)			#Don't know why this isn't working, sending a legit audio file
+		post_facebook_audio(sender_id, audiolink)			#Don't know why this isn't working, sending a legit audio file
+		print '^'*15 + '\n'
+
 		bestaudio = video.getbestaudio(preftype='m4a')
 		post_facebook_audio(sender_id, bestaudio.url)
 		message_text = 'Download audio at 320kbps bitrate:\n\n' + audiolink
